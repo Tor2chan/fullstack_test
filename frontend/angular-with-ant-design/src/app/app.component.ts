88e968ce17d1dd59643e8ac029451e2dcfc1636d
Tor2chan <lord2505@hotmail.com>
2024-11-22 14:48:44 +0700

    22/11/24_14:48:PM

--- frontend/angular-with-ant-design/src/app/app.component.ts
+++ frontend/angular-with-ant-design/src/app/app.component.ts
@@ -2,28 +2,34 @@ import { Component, OnInit } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { CommonModule } from '@angular/common';
 import { TestService } from './services/test.service';
+import { User, UserService } from './services/user.service';
+
 @Component({
   selector: 'app-root',
   standalone: true,
   templateUrl: './app.component.html',
-  imports: [RouterModule, CommonModule]
+  imports: [RouterModule, CommonModule,]
 })
 export class AppComponent implements OnInit {
+  users: User[] = [];
+  newUser: Partial<User> = {};
+
   tests: any[] = [];
 
-  constructor(private testService: TestService) {}
+  constructor(private userService: UserService) {}
 
  
-  ngOnInit() {
-    this.testService.getTests().subscribe(
-      data => {
-        this.tests = data;
-        console.log(data); // ดูข้อมูลที่ได้จาก API
-      },
-      error => {
-        console.error('Error fetching data from API', error);
-      }
-    );
+  ngOnInit(): void {
+    this.userService.getUsers().subscribe((data) => (this.users = data));
+  }
+
+  addUser(): void {
+    if (this.newUser.name && this.newUser.email) {
+      this.userService.addUser(this.newUser as User).subscribe((user) => {
+        this.users.push(user);
+        this.newUser = {};
+      });
+    }
   }
 
 }

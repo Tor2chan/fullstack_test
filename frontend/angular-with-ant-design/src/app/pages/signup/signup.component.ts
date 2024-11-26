bc8f8963829e75cc1c8f06024f00b7394c42b53c
Tor2chan <lord2505@hotmail.com>
2024-11-26 11:45:10 +0700

    26/11/24_11:45:AM

--- frontend/angular-with-ant-design/src/app/pages/signup/signup.component.ts
+++ frontend/angular-with-ant-design/src/app/pages/signup/signup.component.ts
@@ -1,13 +1,8 @@
 import { Component } from '@angular/core';
 import { Router } from '@angular/router';  
 import { CommonModule } from '@angular/common';
-import { 
-  FormBuilder, 
-  FormGroup, 
-  Validators, 
-  ReactiveFormsModule,  
-  FormsModule          
-} from '@angular/forms';
+import { FormBuilder, FormGroup, Validators, ReactiveFormsModule, FormsModule} from '@angular/forms';
+import { UserService, User } from '../../services/user.service';
 
 @Component({
   selector: 'app-login  ',
@@ -23,8 +18,10 @@ import {
 export class SignupComponent {
   loginForm: FormGroup;
   submitted = false;  
+  users: User[] = [];
+  newUser: Partial<User> = {};
 
-  constructor(private fb: FormBuilder) {
+  constructor(private fb: FormBuilder, private userService: UserService, private router: Router) {
     this.loginForm = this.fb.group({
       email: ['', Validators.required],
       username: ['', Validators.required],
@@ -39,4 +36,34 @@ export class SignupComponent {
       console.log('Form submitted:', this.loginForm.value);
     }
   }
+
+  ngOnInit(): void {
+    this.userService.getUsers().subscribe((data) => (this.users = data));
+  }
+
+  home_redirect(){
+    this.router.navigate(['home']);
+  }
+
+  addUser(): void {
+    if (this.newUser.name && this.newUser.email && this.newUser.username && this.newUser.password) {
+  
+      if (!this.newUser.role) {
+        this.newUser.role = "user";
+      }
+  
+      this.userService.addUser(this.newUser as User).subscribe({
+        next: (user) => {
+          this.users.push(user);
+          this.newUser = {};
+          this.home_redirect();
+        },
+        error: (error) => {
+          console.error('Error adding user:', error);
+        }
+      });
+    }
+  }
+
+
 }
